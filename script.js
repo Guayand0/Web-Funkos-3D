77ad2c9419ea7bc5a508d892f9addcb4d6c9e220
David Beamonde <179389440+Guayand0@users.noreply.github.com>
2025-12-23 20:50:47 +0100

    0.0.4

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 const galeria = document.getElementById('galeria');
 
 fetch('funkos/funkos.json')
-    .then(response => response.json())
-    .then(funkos => {
-        funkos.forEach(f => {
-            const card = document.createElement('div');
-            card.className = 'funko-card';
+  .then(res => res.json())
+  .then(funkos => {
+    funkos.forEach(f => {
+      const card = document.createElement('div');
+      card.className = 'funko-card';
 
-            const img = document.createElement('img');
-            img.src = `funkos/${f.carpeta}/${f.imagen}`;
-            card.appendChild(img);
+      const img = document.createElement('img');
+      let currentIndex = 0;
+      img.src = `funkos/${f.carpeta}/${f.imagenes[currentIndex]}`;
+      card.appendChild(img);
 
-            const titulo = document.createElement('h2');
-            titulo.textContent = f.nombre;
-            card.appendChild(titulo);
+      const left = document.createElement('div');
+      left.className = 'arrow arrow-left';
+      left.innerHTML = '&#10094;';
+      left.onclick = () => {
+        currentIndex = (currentIndex - 1 + f.imagenes.length) % f.imagenes.length;
+        img.src = `funkos/${f.carpeta}/${f.imagenes[currentIndex]}`;
+      }
+      card.appendChild(left);
 
-            const desc = document.createElement('p');
-            desc.textContent = f.descripcion;
-            card.appendChild(desc);
+      const right = document.createElement('div');
+      right.className = 'arrow arrow-right';
+      right.innerHTML = '&#10095;';
+      right.onclick = () => {
+        currentIndex = (currentIndex + 1) % f.imagenes.length;
+        img.src = `funkos/${f.carpeta}/${f.imagenes[currentIndex]}`;
+      }
+      card.appendChild(right);
 
-            galeria.appendChild(card);
-        });
-    })
-    .catch(err => console.error("Error cargando los Funkos:", err));
+      const titulo = document.createElement('h2');
+      titulo.textContent = f.nombre;
+      card.appendChild(titulo);
+
+      const desc = document.createElement('p');
+      desc.textContent = f.descripcion;
+      card.appendChild(desc);
+
+      galeria.appendChild(card);
+    });
+  });
